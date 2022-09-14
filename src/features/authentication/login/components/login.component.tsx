075e39403c05cec8bfc3acc1f3eb81f67ab3522c
Kapil Bastola <kapilmuni209@gmail.com>
2022-09-14 22:24:03 +0545

    added dark mode to both login and registration pages

--- src/features/authentication/login/components/login.component.tsx
+++ src/features/authentication/login/components/login.component.tsx
@@ -1,3 +1,4 @@
+import { CircularLoader } from '../../../../components/loader/circular-loader.component'
 import { ILoginProps } from './login.container'
 
 export const Login = ({ ...props }: ILoginProps) => {
@@ -17,19 +18,19 @@ export const Login = ({ ...props }: ILoginProps) => {
 
     if (showError) {
         errorMessage = (
-            <p className='text-red-600 text-center animate-slide-right h-fit bg-gray-100 w-20vw rounded py-4 px-8 last:text-sm font-semibold mt-4 md:absolute top-24 right-10'>
+            <p className='text-red-600 dark:bg-transparent dark:border dark:border-red-600 dark:text-textDark1 text-center animate-slide-right h-fit bg-gray-100 w-20vw rounded py-4 px-8 last:text-sm font-semibold mt-4 md:absolute top-44 right-10'>
                 {error?.message}
             </p>
         )
     }
 
     return (
-        <div className='w-4/5 m-auto bg-white h-full relative'>
-            <div className='flex flex-col items-center justify-start my-24'>
+        <div className='w-4/5 m-auto bg-white h-full relative dark:w-full dark:bg-bgDark dark:h-screen'>
+            <div className='flex flex-col items-center justify-start py-24'>
                 <p className='text-4xl font-bold drop-shadow-lg text-orange-600'>
                     P<span className='text-6xl'>€</span>T
                 </p>
-                <p className='text-sm text-gray-600 mt-3'>
+                <p className='text-sm text-gray-600 mt-3 dark:text-textDark2'>
                     Are you new ?
                     <span
                         className='font-bold text-orange-600 underline cursor-pointer ml-2'
@@ -42,66 +43,70 @@ export const Login = ({ ...props }: ILoginProps) => {
                     </span>
                 </p>
                 <div
-                    className='bg-tealdark p-8 rounded-md shadow-md text-white z-10  my-2'
+                    className='bg-tealdark dark:bg-hoverDark p-8 rounded-md shadow-md text-white z-10  my-2'
                     aria-label='login form'
                 >
-                    <p className='text-dark text-2xl text-center'>Sign In</p>
-                    <p className='text-green-300 text-md text-center mt-2'>
+                    <p className='text-dark text-2xl text-center dark:text-orange-400'>Sign In</p>
+                    <p className='text-green-300 text-md text-center mt-2 dark:text-textDark2'>
                         Please provide your credentials
                     </p>
-                    {isLoading ? (
-                        <p className='text-center text-white'>Logging in...</p>
-                    ) : (
-                        <form
-                            className='flex flex-col items-center justify-center mt-4'
-                            onSubmit={handleLogin}
-                        >
-                            <div className='flex items-center mt-4'>
-                                <label
-                                    className='mr-4 w-16'
-                                    htmlFor='email'
-                                >
-                                    Email
-                                </label>
-                                <input
-                                    id='email'
-                                    name='email'
-                                    className='p-2 rounded text-black'
-                                    type='email'
-                                    placeholder='email@address.com'
-                                    value={formState.email}
-                                    onChange={handleChange}
-                                    onFocus={handleFocus}
-                                />
-                            </div>
-                            <div className='flex items-center mt-4'>
-                                <label
-                                    className='mr-4 w-16'
-                                    htmlFor='password'
-                                >
-                                    Password
-                                </label>
-                                <input
-                                    id='password'
-                                    name='password'
-                                    className='p-2 rounded text-black'
-                                    type='password'
-                                    placeholder='******'
-                                    value={formState.password}
-                                    onChange={handleChange}
-                                    onFocus={handleFocus}
-                                />
-                            </div>
+
+                    <form
+                        className='flex flex-col items-center justify-center mt-4'
+                        onSubmit={handleLogin}
+                    >
+                        <div className='flex items-center mt-4'>
+                            <label
+                                className='mr-4 w-16'
+                                htmlFor='email'
+                            >
+                                Email
+                            </label>
+                            <input
+                                id='email'
+                                name='email'
+                                className='p-2 rounded text-black'
+                                type='email'
+                                placeholder='email@address.com'
+                                value={formState.email}
+                                onChange={handleChange}
+                                onFocus={handleFocus}
+                            />
+                        </div>
+                        <div className='flex items-center mt-4'>
+                            <label
+                                className='mr-4 w-16'
+                                htmlFor='password'
+                            >
+                                Password
+                            </label>
+                            <input
+                                id='password'
+                                name='password'
+                                className='p-2 rounded text-black'
+                                type='password'
+                                placeholder='******'
+                                value={formState.password}
+                                onChange={handleChange}
+                                onFocus={handleFocus}
+                            />
+                        </div>
+                        <div className='flex items-center mt-6'>
                             <button
-                                className='bg-orange-600 w-fit mt-6 py-2 disabled:bg-gray-200 disabled:text-gray-400 px-8 rounded shadow-xl'
+                                className='bg-orange-600 dark:bg-transparent dark:border dark:border-green-600 dark:hover:bg-green-600 w-fit py-2 disabled:bg-gray-200 disabled:text-gray-400 px-8 rounded shadow-xl dark:disabled:bg-transparent dark:disabled:border-borderDark'
                                 aria-live='polite'
                                 type='submit'
-                                disabled={Boolean(showError)}
+                                disabled={Boolean(showError) || isLoading}
                             >
                                 Sign In
                             </button>
-                        </form>
-                    )}
+                            {isLoading ? (
+                                <div className='ml-4'>
+                                    <CircularLoader />
+                                </div>
+                            ) : null}
+                        </div>
+                    </form>
                 </div>
             </div>
             {errorMessage}
--- src/features/authentication/register/components/register.component.tsx
+++ src/features/authentication/register/components/register.component.tsx
@@ -1,3 +1,5 @@
+import { CircularLoader } from '../../../../components/loader/circular-loader.component'
+import { Success } from '../../../../components/success/success.component'
 import { IRegisterProps } from './register.container'
 
 // This component file just returns the view without much logical handling
@@ -20,35 +22,32 @@ export const Register = ({ ...props }: IRegisterProps) => {
 
     if (showError) {
         errorMessage = (
-            <p className='text-red-600 text-center animate-slide-right h-fit bg-gray-100 rounded px-8 py-4 w-20vw text-sm font-semibold mt-4 md:absolute top-24 right-10'>
+            <p className='text-red-600 text-center animate-slide-right h-fit bg-gray-100 dark:bg-transparent dark:border-red-600 dark:border dark:text-textDark1 rounded px-8 py-4 w-20vw text-sm font-semibold mt-4 md:absolute top-40 right-10'>
                 {error?.message}
             </p>
         )
     }
 
     const successMessage = (
-        <div className='bg-teallight p-5 my-4 rounded text-center w-30vw'>
-            <p className='text-green-600 text-xl font-semibold'>Account Created Successfully</p>
-            <p className='text-gray-700 text-sm my-1'>
+        <div className='bg-teallight dark:bg-bgDark p-5 my-4 rounded text-center w-30vw'>
+            <Success />
+            <p className='text-green-600 dark:text-green-500 text-xl font-semibold'>
+                Account Created Successfully
+            </p>
+            <p className='text-gray-700 text-sm my-1 dark:text-textDark2'>
                 An email has been sent for verfication.
                 <br /> Please verify it to continue.
             </p>
         </div>
     )
 
-    const loadingMessage = (
-        <p className='text-center bg-orange-600 text-lg text-white px-1 py-1 my-2 rounded'>
-            Creating your account...
-        </p>
-    )
-
     return (
-        <div className='w-4/5 m-auto bg-white h-full relative'>
-            <div className='flex flex-col items-center justify-start my-16'>
+        <div className='w-4/5 m-auto bg-white h-full relative dark:w-full dark:bg-bgDark dark:h-screen'>
+            <div className='flex flex-col items-center justify-start py-16'>
                 <p className='text-4xl font-bold drop-shadow-lg text-orange-600'>
                     P<span className='text-6xl'>€</span>T
                 </p>
-                <p className='text-sm text-gray-600 mt-3'>
+                <p className='text-sm text-gray-600 mt-3 dark:text-textDark2'>
                     Already have an account ?
                     <span
                         className='font-bold text-orange-600 underline cursor-pointer ml-2'
@@ -61,16 +60,14 @@ export const Register = ({ ...props }: IRegisterProps) => {
                     </span>
                 </p>
                 <div
-                    className='bg-tealdark p-8 rounded-md shadow-md text-white mt-2 z-10'
+                    className='bg-tealdark dark:bg-hoverDark p-8 rounded-md shadow-md text-white mt-2 z-10'
                     aria-label='registration form'
                 >
-                    <p className='text-dark text-2xl text-center'>Sign Up</p>
-                    <p className='text-green-300 text-md text-center mt-2'>
+                    <p className='text-dark text-2xl text-center dark:text-orange-400'>Sign Up</p>
+                    <p className='text-green-300 text-md text-center mt-2 dark:text-textDark2'>
                         We are eager to help your with your expense tracking
                     </p>
-                    {isLoading ? (
-                        loadingMessage
-                    ) : success ? (
+                    {success ? (
                         successMessage
                     ) : (
                         <form
@@ -131,14 +128,21 @@ export const Register = ({ ...props }: IRegisterProps) => {
                                     onFocus={handleFocus}
                                 />
                             </div>
-                            <button
-                                className='bg-orange-600 w-fit mt-8 py-2 px-8 disabled:bg-gray-200 disabled:text-gray-400 rounded shadow-xl'
-                                aria-live='polite'
-                                type='submit'
-                                disabled={Boolean(showError)}
-                            >
-                                Sign Up
-                            </button>
+                            <div className='flex items-center mt-8'>
+                                <button
+                                    className='bg-orange-600 dark:bg-transparent dark:border dark:disabled:hover:bg-transparent dark:border-green-600 dark:hover:bg-green-600 w-fit py-2 px-8 disabled:bg-gray-200 disabled:text-gray-400 rounded shadow-xl dark:disabled:text-textDark2 dark:disabled:border-borderDark'
+                                    aria-live='polite'
+                                    type='submit'
+                                    disabled={Boolean(showError) || isLoading || success}
+                                >
+                                    Sign Up
+                                </button>
+                                {isLoading ? (
+                                    <div className='ml-4'>
+                                        <CircularLoader />
+                                    </div>
+                                ) : null}
+                            </div>
                         </form>
                     )}
                 </div>
